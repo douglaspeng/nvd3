d6030332b81253a1c5560879c0a6d8cd71d5bb21
douglas peng <douglas.peng@pubmatic.com>
2014-06-26 16:07:53 -0700

    bug fixed

--- nv.d3.js
+++ nv.d3.js
@@ -133,44 +133,44 @@ the rectangle. The dispatch is given one object which contains the mouseX/Y loca
 It also has 'pointXValue', which is the conversion of mouseX to the x-axis scale.
 */
 nv.interactiveGuideline = function() {
-	"use strict";
-	var tooltip = nv.models.tooltip();
-	//Public settings
-	var width = null
-	, height = null
+  "use strict";
+  var tooltip = nv.models.tooltip();
+  //Public settings
+  var width = null
+  , height = null
     //Please pass in the bounding chart's top and left margins
     //This is important for calculating the correct mouseX/Y positions.
-	, margin = {left: 0, top: 0}
-	, xScale = d3.scale.linear()
-	, yScale = d3.scale.linear()
-	, dispatch = d3.dispatch('elementMousemove', 'elementMouseout','elementDblclick')
-	, showGuideLine = true
-	, svgContainer = null  
+  , margin = {left: 0, top: 0}
+  , xScale = d3.scale.linear()
+  , yScale = d3.scale.linear()
+  , dispatch = d3.dispatch('elementMousemove', 'elementMouseout','elementDblclick')
+  , showGuideLine = true
+  , svgContainer = null  
     //Must pass in the bounding chart's <svg> container.
     //The mousemove event is attached to this container.
-	;
+  ;
 
-	//Private variables
-	var isMSIE = navigator.userAgent.indexOf("MSIE") !== -1  //Check user-agent for Microsoft Internet Explorer.
-	;
+  //Private variables
+  var isMSIE = navigator.userAgent.indexOf("MSIE") !== -1  //Check user-agent for Microsoft Internet Explorer.
+  ;
 
 
-	function layer(selection) {
-		selection.each(function(data) {
-				var container = d3.select(this);
-				
-				var availableWidth = (width || 960), availableHeight = (height || 400);
+  function layer(selection) {
+    selection.each(function(data) {
+        var container = d3.select(this);
+        
+        var availableWidth = (width || 960), availableHeight = (height || 400);
 
-				var wrap = container.selectAll("g.nv-wrap.nv-interactiveLineLayer").data([data]);
-				var wrapEnter = wrap.enter()
-								.append("g").attr("class", " nv-wrap nv-interactiveLineLayer");
-								
-				
-				wrapEnter.append("g").attr("class","nv-interactiveGuideLine");
-				
-				if (!svgContainer) {
-					return;
-				}
+        var wrap = container.selectAll("g.nv-wrap.nv-interactiveLineLayer").data([data]);
+        var wrapEnter = wrap.enter()
+                .append("g").attr("class", " nv-wrap nv-interactiveLineLayer");
+                
+        
+        wrapEnter.append("g").attr("class","nv-interactiveGuideLine");
+        
+        if (!svgContainer) {
+          return;
+        }
 
                 function mouseHandler() {
                       var d3mouse = d3.mouse(this);
@@ -202,7 +202,7 @@ nv.interactiveGuideline = function() {
                             subtractMargin = false;
 
                          if (d3.event.target.className.baseVal.match("nv-legend"))
-                         	mouseOutAnyReason = true;
+                          mouseOutAnyReason = true;
                           
                       }
 
@@ -220,13 +220,13 @@ nv.interactiveGuideline = function() {
                         || mouseOutAnyReason
                         ) 
                       {
-                      		if (isMSIE) {
-                      			if (d3.event.relatedTarget 
-                      				&& d3.event.relatedTarget.ownerSVGElement === undefined
-                      				&& d3.event.relatedTarget.className.match(tooltip.nvPointerEventsClass)) {
-                      				return;
-                      			}
-                      		}
+                          if (isMSIE) {
+                            if (d3.event.relatedTarget 
+                              && d3.event.relatedTarget.ownerSVGElement === undefined
+                              && d3.event.relatedTarget.className.match(tooltip.nvPointerEventsClass)) {
+                              return;
+                            }
+                          }
                             dispatch.elementMouseout({
                                mouseX: mouseX,
                                mouseY: mouseY
@@ -252,75 +252,75 @@ nv.interactiveGuideline = function() {
                       }
                 }
 
-				svgContainer
-				      .on("mousemove",mouseHandler, true)
-				      .on("mouseout" ,mouseHandler,true)
+        svgContainer
+              .on("mousemove",mouseHandler, true)
+              .on("mouseout" ,mouseHandler,true)
                       .on("dblclick" ,mouseHandler)
-				      ;
+              ;
 
-				 //Draws a vertical guideline at the given X postion.
-				layer.renderGuideLine = function(x) {
-				 	if (!showGuideLine) return;
-				 	var line = wrap.select(".nv-interactiveGuideLine")
-				 	      .selectAll("line")
-				 	      .data((x != null) ? [nv.utils.NaNtoZero(x)] : [], String);
+         //Draws a vertical guideline at the given X postion.
+        layer.renderGuideLine = function(x) {
+          if (!showGuideLine) return;
+          var line = wrap.select(".nv-interactiveGuideLine")
+                .selectAll("line")
+                .data((x != null) ? [nv.utils.NaNtoZero(x)] : [], String);
 
-				 	line.enter()
-				 		.append("line")
-				 		.attr("class", "nv-guideline")
-				 		.attr("x1", function(d) { return d;})
-				 		.attr("x2", function(d) { return d;})
-				 		.attr("y1", availableHeight)
-				 		.attr("y2",0)
-				 		;
-				 	line.exit().remove();
+          line.enter()
+            .append("line")
+            .attr("class", "nv-guideline")
+            .attr("x1", function(d) { return d;})
+            .attr("x2", function(d) { return d;})
+            .attr("y1", availableHeight)
+            .attr("y2",0)
+            ;
+          line.exit().remove();
 
-				}
-		});
-	}
+        }
+    });
+  }
 
-	layer.dispatch = dispatch;
-	layer.tooltip = tooltip;
+  layer.dispatch = dispatch;
+  layer.tooltip = tooltip;
 
-	layer.margin = function(_) {
-	    if (!arguments.length) return margin;
-	    margin.top    = typeof _.top    != 'undefined' ? _.top    : margin.top;
-	    margin.left   = typeof _.left   != 'undefined' ? _.left   : margin.left;
-	    return layer;
+  layer.margin = function(_) {
+      if (!arguments.length) return margin;
+      margin.top    = typeof _.top    != 'undefined' ? _.top    : margin.top;
+      margin.left   = typeof _.left   != 'undefined' ? _.left   : margin.left;
+      return layer;
     };
 
-	layer.width = function(_) {
-		if (!arguments.length) return width;
-		width = _;
-		return layer;
-	};
+  layer.width = function(_) {
+    if (!arguments.length) return width;
+    width = _;
+    return layer;
+  };
 
-	layer.height = function(_) {
-		if (!arguments.length) return height;
-		height = _;
-		return layer;
-	};
+  layer.height = function(_) {
+    if (!arguments.length) return height;
+    height = _;
+    return layer;
+  };
 
-	layer.xScale = function(_) {
-		if (!arguments.length) return xScale;
-		xScale = _;
-		return layer;
-	};
+  layer.xScale = function(_) {
+    if (!arguments.length) return xScale;
+    xScale = _;
+    return layer;
+  };
 
-	layer.showGuideLine = function(_) {
-		if (!arguments.length) return showGuideLine;
-		showGuideLine = _;
-		return layer;
-	};
+  layer.showGuideLine = function(_) {
+    if (!arguments.length) return showGuideLine;
+    showGuideLine = _;
+    return layer;
+  };
 
-	layer.svgContainer = function(_) {
-		if (!arguments.length) return svgContainer;
-		svgContainer = _;
-		return layer;
-	};
+  layer.svgContainer = function(_) {
+    if (!arguments.length) return svgContainer;
+    svgContainer = _;
+    return layer;
+  };
 
 
-	return layer;
+  return layer;
 };
 
 /* Utility class that uses d3.bisect to find the index in a given array, where a search value can be inserted.
@@ -337,7 +337,7 @@ Has the following known issues:
    * Won't work if there are duplicate x coordinate values.
 */
 nv.interactiveBisect = function (values, searchVal, xAccessor) {
-	  "use strict";
+    "use strict";
       if (! values instanceof Array) return null;
       if (typeof xAccessor !== 'function') xAccessor = function(d,i) { return d.x;}
 
@@ -3222,11 +3222,7 @@ nv.models.cumulativeLineChart = function() {
       if (!line.values) {
          return line;
       }
-      var indexValue = line.values[idx];
-      if (indexValue == null) {
-        return line;
-      }
-      var v = lines.y()(indexValue, idx);
+      var v = lines.y()(line.values[idx], idx);
 
       //TODO: implement check below, and disable series if series loses 100% or more cause divide by 0 issue
       if (v < -.95 && !noErrorCheck) {
@@ -4650,7 +4646,8 @@ nv.models.indentedTree = function() {
 
             d3.select(this).select('span')
               .attr('class', d3.functor(column.classes) )
-              .text(function(d) { return column.format ? (d[column.key] ? column.format(d[column.key]) : '-') :  (d[column.key] || '-'); });
+              .text(function(d) { return column.format ? column.format(d) :
+                                        (d[column.key] || '-') });
           });
 
         if  (column.showCount) {
@@ -5544,7 +5541,7 @@ nv.models.lineChart = function() {
       if (showLegend) {
         legend.width(availableWidth);
 
-        if (invisibleLegend) {
+        if (invisibleLegend){
           g.select('.nv-legendWrap')
             .attr('style','display:none')
             .datum(data)
@@ -5552,12 +5549,11 @@ nv.models.lineChart = function() {
         }
         else {
           g.select('.nv-legendWrap')
-            .datum(data)
-            .call(legend);
+              .datum(data)
+              .call(legend);
         }
-
         if ( margin.top != legend.height()) {
-          if (invisibleLegend != true){
+          if (!invisibleLegend){
             margin.top = legend.height();
           }
           availableHeight = (height || parseInt(container.style('height')) || 400)
@@ -5865,7 +5861,6 @@ nv.models.lineChart = function() {
     transitionDuration = _;
     return chart;
   };
-
   chart.invisibleLegend = function(_) {
     if (!arguments.length) return invisibleLegend;
     invisibleLegend = _;
@@ -12965,7 +12960,7 @@ nv.models.sparkline = function() {
               var yValues = data.map(function(d, i) { return getY(d,i); });
               function pointIndex(index) {
                   if (index != -1) {
-	              var result = data[index];
+                var result = data[index];
                       result.pointIndex = index;
                       return result;
                   } else {
@@ -13738,9 +13733,9 @@ nv.models.stackedArea = function() {
   };
 
   chart.interpolate = function(_) {
-	    if (!arguments.length) return interpolate;
-	    interpolate = _;
-	    return chart;
+      if (!arguments.length) return interpolate;
+      interpolate = _;
+      return chart;
   };
   //============================================================
 
